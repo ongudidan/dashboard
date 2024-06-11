44af72fe2f97aa45ead43f9a7516d6779bc8c959
ongudidan <ongudidan@gmail.com>
2024-06-11 19:39:59 +0300

    first commit

---
+++ editNavbar.php
@@ -0,0 +1,136 @@
+<?php
+
+include("modelClass.php");
+
+$database->query('SELECT * FROM navbar WHERE id = 1;');
+$row = $database->single();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- create-post.html  21 Nov 2019 04:05:02 GMT -->
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Otika - Admin Dashboard Template</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="assets/css/app.min.css">
+  <link rel="stylesheet" href="assets/bundles/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="assets/bundles/jquery-selectric/selectric.css">
+  <link rel="stylesheet" href="assets/bundles/bootstrap-tagsinput/dist/bootstrap-tagsinput.css">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/components.css">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
+</head>
+
+<body>
+  <div class="loader"></div>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+
+      <!-- NAVBAR SECTION  -->
+      <?php
+      include("navbar.php");
+      ?>
+
+      <?php
+      include("main-sidebar.php");
+      ?>
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-body">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Edit the navigation bar</h4>
+                  </div>
+                  <form id="" action="post.php" method="POST" enctype="multipart/form-data">
+                    <div class="card-body">
+                      <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Call us at</label>
+                        <div class="col-sm-12 col-md-7">
+                          <input name="phone" value="<?= $row['phone']; ?>" type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Email address</label>
+                        <div class="col-sm-12 col-md-7">
+                          <input name="email" value="<?= $row['email']; ?>" type="email" class="form-control">
+                        </div>
+                      </div>
+                      <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Physical address</label>
+                        <div class="col-sm-12 col-md-7">
+                          <input name="physical_address" value="<?= $row['physical_address']; ?>" type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="form-group row mb-4">
+                        <label name="logo" class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Logo</label>
+                        <div class="col-sm-12 col-md-7">
+                          <img class="dropdown-item-avatar" src="<?= $row['logoDestination']; ?>" alt="">
+
+                          <div id="image-preview" class="image-preview">
+                            <label for="image-upload" id="image-label">Choose File</label>
+                            <input type="file" name="logo" id="image-upload" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                        <div class="col-sm-12 col-md-7">
+                          <button type="submit" name="submit" id="swal-2" class="btn btn-primary">Save changes</button>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <!-- SETTING SIDEBAR SECTION  -->
+
+        <?php
+        include("setting-sidebar.php");
+        ?>
+      </div>
+      <!-- FOOTER SECTION  -->
+      <?php
+      include("main-footer.php");
+      ?>
+    </div>
+  </div>
+  <script src="jquery-3.7.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.5/dist/sweetalert2.all.min.js"></script>
+  <script src="form.js"></script>
+  <!-- General JS Scripts -->
+  <script src="assets/js/app.min.js"></script>
+  <!-- JS Libraies -->
+  <script src="assets/bundles/summernote/summernote-bs4.js"></script>
+  <script src="assets/bundles/jquery-selectric/jquery.selectric.min.js"></script>
+  <script src="assets/bundles/upload-preview/assets/js/jquery.uploadPreview.min.js"></script>
+  <script src="assets/bundles/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js"></script>
+  <!-- Page Specific JS File -->
+  <script src="assets/js/page/create-post.js"></script>
+  <!-- Template JS File -->
+  <script src="assets/js/scripts.js"></script>
+  <!-- Custom JS File -->
+  <script src="assets/js/custom.js"></script>
+</body>
+
+
+<!-- create-post.html  21 Nov 2019 04:05:03 GMT -->
+
+</html>
---
+++ main-sidebar.php
@@ -0,0 +1,19 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.php"> <img alt="image" src="assets/img/logo.png" class="header-logo" /> <span class="logo-name">Otika</span>
+            </a>
+        </div>
+        <ul class="sidebar-menu">
+
+            <li class="menu-header">Otika</li>
+            <li class="dropdown active">
+                <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="layout"></i><span>HOME PAGE</span></a>
+                <ul class="dropdown-menu">
+                    <li class="active"><a class="nav-link" href="editNavbar.php">Navigation bar</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </aside>
+</div>
